263e2e7d9f86414aa34f900060a8569b8cfc67a8
Paddy ✔️ <paddyjo2011@hotmail.co.uk>
2020-08-27 17:12:06 +0100

    Stats raw fetch request for debugging

--- api/api.js
+++ api/api.js
@@ -1,4 +1,5 @@
 const fetch = require('node-fetch')
+const fs = require('fs')
 
 module.exports = class api {
   constructor(){
@@ -17,7 +18,7 @@ module.exports = class api {
       "startDate": 0,
       "endDate": 0,
       "owners": [id.toString()],
-      "stats": ["s11_social_bp_level", "s13_social_bp_level"]
+      "stats": ["s11_social_bp_level", "s13_social_bp_level", "s14_social_bp_level"]
   }
     return fetch(`https://statsproxy-public-service-live.ol.epicgames.com/statsproxy/api/statsv2/query`, {
       headers: {
@@ -29,6 +30,20 @@ module.exports = class api {
     }).then(r => r.json())
     .then(r => r)
   }
+
+  async stastRaw(id){
+
+    return fetch(`https://statsproxy-public-service-live.ol.epicgames.com/statsproxy/api/statsv2/account/${id}`, {
+      headers: {
+        'Content-Type': 'application/json',
+        "Authorization": `bearer ${api.fnClient.authenticator.accessToken}`
+      },
+      method: "GET"
+    }).then(r => r.json())
+    .then(r => r)
+  }
+
+  
 
 }
 
